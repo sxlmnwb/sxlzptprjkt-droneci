659801ce49837872b177c908ce68a0785329890b
Jody Yuantoro <xyzuannihboss@gmail.com>
2022-08-11 15:55:57 +0000

    initial: CI build tamplate
    Signed-off-by: Jody Yuantoro <xyzuannihboss@gmail.com>
    Signed-off-by: Salman Wahib <sxlmnwb.dev@gmail.com>

--- llvm-tc_build.sh
+++ llvm-tc_build.sh
@@ -1,5 +1,9 @@
-#!/usr/bin/env bash
-# Use xRageTC build script as LLVM Build Script.
-git clone https://github.com/xyz-prjkt/llvmTC $(pwd)/llvmTC -b llvm-tc
-cd $(pwd)/llvmTC
+#
+# // Copyright (C) 2021 Jody Yuantoro (xyzuan) Aarch64
+# // Copyright (C) 2022 Salman Wahib (sxlmnwb) x86_64
+#
+
+# Use sxlzptprjkt toolchain build
+git clone https://github.com/sxlmnwb/sxlzptprjkt-toolchain-build $(pwd)/toolchain -b main
+cd $(pwd)/toolchain
 bash build-tc.sh
